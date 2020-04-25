9ac394c1235a6e316aa76f663192c22d2c9bb4bf
thiagomilton.f@gmail.com <thiagomilton.f@gmail.com>
2020-04-25 10:42:48 -0300

    finalizado controller encerrar patio

--- app/Http/Controllers/exitPatio.php
+++ app/Http/Controllers/exitPatio.php
@@ -2,30 +2,22 @@
 
 namespace App\Http\Controllers;
 use App\patio;
+use Auth;
 
 use Illuminate\Http\Request;
 
 class exitPatio extends Controller
 {
-    public function exit(patio $modelPatio,Request $request)
+    public function exit(patio $modelPatio)
     {
         
         date_default_timezone_set('America/Sao_Paulo');
         $dateAtual = date("Y-m-d");
-        //var_dump($dateAtual);
-        //exit;
+
         $lastIdPatio = $modelPatio->max('id');
         $patio = $modelPatio->find($lastIdPatio);
         $update = $patio->where('data','=',$dateAtual)->whereNull('dataFinal')->update(['dataFinal'=>$dateAtual]);
-        $value = $request->session()->all();
-
-        //var_dump($_COOKIE);
-        //var_dump($value);
-        //["PHPSESSID"]
-        //unset($_COOKIE["PHPSESSID"]);
-
-        //exit;
-
-        //return redirect()->route('login');
+        Auth::logout();
+        return redirect()->route('login');
     }
 }
